2c81e574b38e948903547e4852d8f8d1daa90adc
zkwbbr <45949485+zkwbbr@users.noreply.github.com>
2018-12-19 04:47:14 +0800

    Add unit tests for Utils::randomToken()

---
+++ tests/unit/UtilsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use \PHPUnit\Framework\TestCase;
+use \MetaRush\OtpAuth\Utils;
+
+class UtilsTest extends TestCase
+{
+    public function testRandomTokenLength()
+    {
+        $length = 9;
+        $test = Utils::randomToken($length);
+        $test = mb_strlen($test);
+        $this->assertEquals($length, $test);
+    }
+
+    public function testRandomTokenPool()
+    {
+        $length = 9;
+
+        $pool = 'abc';
+        $pattern = '~([a-z])+~';
+        $test = Utils::randomToken($length, $pool);
+        $this->assertRegExp($pattern, $test);
+
+        $pool = 'ABC';
+        $pattern = '~([A-Z])+~';
+        $test = Utils::randomToken($length, $pool);
+        $this->assertRegExp($pattern, $test);
+
+        $pool = '123';
+        $pattern = '~([0-9])+~';
+        $test = Utils::randomToken($length, $pool);
+        $this->assertRegExp($pattern, $test);
+
+        $pool = 'xyzXYZ123';
+        $pattern = '~([a-zA-Z0-9])+~';
+        $test = Utils::randomToken($length, $pool);
+        $this->assertRegExp($pattern, $test);
+    }
+}
