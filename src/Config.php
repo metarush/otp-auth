a81b2e8f9b594f402d21efb82400500d9c3e81c1
zkwbbr <45949485+zkwbbr@users.noreply.github.com>
2019-01-24 01:55:43 +0800

    Add Mailer class

--- src/Config.php
+++ src/Config.php
@@ -4,11 +4,130 @@ namespace MetaRush\OtpAuth;
 
 class Config
 {
+    private $fromName;
+    private $fromEmail;
+    private $subject = "Here's your OTP";
+    private $body = "{OTP}\r\n\r\nNote: This OTP is valid for 5 minutes";
+    private $transport;
     private $table;
     private $usernameColumn;
     private $emailColumn;
     private $otpLength = 8;
     private $characterPool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    /**
+     * Get the From Name of the OTP message
+     *
+     * @return string|null
+     */
+    public function getFromName(): ?string
+    {
+        return $this->fromName;
+    }
+
+    /**
+     * Set the From Name of the OTP message
+     *
+     * @param string $fromName
+     * @return $this
+     */
+    public function setFromName(string $fromName)
+    {
+        $this->fromName = $fromName;
+        return $this;
+    }
+
+    /**
+     * Get the From Email of the OTP message
+     *
+     * @return string
+     */
+    public function getFromEmail(): string
+    {
+        return $this->fromEmail;
+    }
+
+    /**
+     * Set the From Email of the OTP message
+     *
+     * @param string $fromEmail
+     * @return $this
+     */
+    public function setFromEmail(string $fromEmail)
+    {
+        $this->fromEmail = $fromEmail;
+
+        return $this;
+    }
+
+    /**
+     * Get the SwiftMailer transport object that will be used to send email
+     *
+     * @return \Swift_Transport
+     */
+    public function getTransport(): \Swift_Transport
+    {
+        return $this->transport;
+    }
+
+    /**
+     * Set the SwiftMailer transport object that will be used to send email
+     *
+     * @link https://swiftmailer.symfony.com/docs/sending.html#transport-types
+     * @param \Swift_Transport $transport
+     * @return $this
+     */
+    public function setTransport(\Swift_Transport $transport)
+    {
+        $this->transport = $transport;
+
+        return $this;
+    }
+
+    /**
+     * Get subject of the OTP email
+     *
+     * @return string
+     */
+    public function getSubject(): string
+    {
+        return $this->subject;
+    }
+
+    /**
+     * Set subject of the OTP email
+     *
+     * @param string $subject
+     * @return $this
+     */
+    public function setSubject(string $subject)
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    /**
+     * Get body of the OTP email
+     *
+     * @return string
+     */
+    public function getBody(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set body of the OTP email
+     *
+     * @param string $body
+     * @return $this
+     */
+    public function setBody(string $body)
+    {
+        $this->message = $body;
+        return $this;
+    }
 
     /**
      * Get the name of table column where email is stored
